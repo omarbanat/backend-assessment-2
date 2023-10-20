a34a0acd31688709e98444f1c2fcd3ee978248e3
jocelynerizk <95102544+jocelynerizk@users.noreply.github.com>
2023-10-20 10:38:41 +0300

    getArticle,deleteArticle, addArticle

--- controller/Articlecontroller.js
+++ controller/Articlecontroller.js
@@ -1,5 +1,5 @@
 const Article = require('../models/Article');
-
+// adding new record article
 const addArticle= async (req,res)=>{
   try {
     const schema=new Article({
@@ -24,4 +24,39 @@ const addArticle= async (req,res)=>{
     });
   }
 }
-module.exports = {addArticle};
+// getting All Articles 
+const getArticle = async (req, res) => {
+  try {
+    const article = await Article.find({});
+    res.status(200).json({
+      success: true,
+      message: 'Data retrieved successfully',
+      data: skill,
+    });
+  } catch (error) {
+    res.status(404).json({
+      success: false,
+      message: 'unable to get data',
+      error: error,
+    });
+  }
+};
+// deletion
+const deleteArticle = async (req, res) => {
+    try {
+      const article = await Article.deleteOne({ _id: req.params.ID });
+      res.status(200).json({
+        success: true,
+        message: 'Article deleted successfully',
+        data: skill,
+      });
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        message: 'Error occured while deleting the article',
+        error: error,
+      });
+    }
+  };
+
+module.exports = {getArticle,deleteArticle, addArticle};
