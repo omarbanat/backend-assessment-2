faffc4cc17cb78fc7ce8e4be255121c80d677d7a
Hassan Fakih <hassan.fakih.80@gmail.com>
2023-10-20 11:25:59 +0300

    finished with bonus

--- models/Article.js
+++ models/Article.js
@@ -3,7 +3,7 @@ const { Schema, model } = mongoose;
 
 const ArticlesSchema = new Schema({
   title: { type: String, required: true, unique: true },
-  category: { type: String, required: true },
+  category: { type: [String], required: true },
   imageURLs: { type: String, required: true },
   date: { type: Date, required: true },
   body: { type: String, required: true },
