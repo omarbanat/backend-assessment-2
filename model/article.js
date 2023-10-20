4530d451d087e87e5e9da726c240fda22388a6c4
jocelynerizk <95102544+jocelynerizk@users.noreply.github.com>
2023-10-20 10:18:20 +0300

    finish model

--- model/article.js
+++ model/article.js
@@ -7,10 +7,10 @@ const articleSchema = new Schema({
   category:{type: String, required:true},
   body:{type: String, required:true},
   author:{type: String, required:true},
-  image:{type: String, required:true},
+  image:{type: String, required:true}
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 });
-const Skill = model('Article',articleSchema);
+const Article= model('Article',articleSchema);
 module.exports = Article;
