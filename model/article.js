48e1927ea5c873dfd4090d0c8d26b6525bd2f143
jocelynerizk <95102544+jocelynerizk@users.noreply.github.com>
2023-10-20 10:15:34 +0300

    creating timestamp

--- model/article.js
+++ model/article.js
@@ -4,10 +4,13 @@ const { Schema, model } = mongoose;
 const articleSchema = new Schema({
   
   name:{type: String, required:true},
-  category:{type: String},
-  body:{type: String},
+  category:{type: String, required:true},
+  body:{type: String, required:true},
   author:{type: String, required:true},
-  image:{type: String}
+  image:{type: String, required:true},
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
 });
 const Skill = model('Article',articleSchema);
 module.exports = Article;
